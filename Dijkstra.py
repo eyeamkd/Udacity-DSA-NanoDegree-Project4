4445b6560069bc18a09e6d115c3066f1bf057b26
Kunal Dubey <kunaldubey2297@gmail.com>
2022-02-18 23:51:14 +0530

    changes to the Dijkstra algorithm

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,15 +1,3 @@
-# algorithm
-# 1. store the final result in a dictionary called "result = {}"
-# 2. Initial distance from the source to all other nodes is infinity
-# 3. set unvisited to contain the nodes that have not been visited until now
-# 4. Create a "Current" dictionary that holds the current path
-# 5. while ( unvisited != empty )
-#       1. Remove the node from the set "unvisited" that has the least distance from the source
-#       2. Unpack the node, and see it's children/ the nodes that it connects to
-#       3. Calculate the distance using the weights of each node that was unpacked
-#       4. If the distance that is calculated is greater than the value that's present in the dictionary,
-#       then update that value
-
 class Node:
     def __init__(self, name):
         self.name = name
@@ -53,24 +41,40 @@ def find_shortest_path():
     unvisited.add('C')
     unvisited.add('D')
     unvisited.add('E')
-    unvisited.add('F')
+    unvisited.add('F') 
+    
+    to_visit = set()
 
-    source_node = node_a
+    source_node = node_a 
+    
+    to_visit.add(source_node) 
+    
+    while len(to_visit) > 0:
+        current_node = to_visit.pop()
+        for child in current_node.children:  
+            if child not in result.keys():
+                result[child] = current_node.children[child]['distance'] 
+                to_visit.add(current_node.children[child]['node'])   
+            else:
+                if result[child] > current_node.children[child]['distance']:
+                    result[child] = current_node.children[child]['distance'] 
+         
+             
 
-    while len(unvisited) > 0:
-        current_node_name = unvisited.pop()
+    # while len(unvisited) > 0:
+    #     current_node_name = unvisited.pop()
 
-        if current_node_name in source_node.children:
-            current_node = source_node.children[current_node_name]['node']
-            result[current_node_name] = source_node.children[current_node_name]['distance']
-            current_node_distance = result[current_node_name]
-            current_node_children = current_node.children
-            for child in current_node_children:
-                if child in result:
-                    if result[child] > current_node_distance + current_node_children[child]['distance']:
-                        result[child] = current_node_distance + current_node_children[child]['distance']
-                else:
-                    result[child] = current_node_distance + current_node_children[child]['distance']
+    #     if current_node_name in source_node.children:
+    #         current_node = source_node.children[current_node_name]['node']
+    #         result[current_node_name] = source_node.children[current_node_name]['distance']
+    #         current_node_distance = result[current_node_name]
+    #         current_node_children = current_node.children
+    #         for child in current_node_children:
+    #             if child in result:
+    #                 if result[child] > current_node_distance + current_node_children[child]['distance']:
+    #                     result[child] = current_node_distance + current_node_children[child]['distance']
+    #             else:
+    #                 result[child] = current_node_distance + current_node_children[child]['distance']
 
     print(result)
 
