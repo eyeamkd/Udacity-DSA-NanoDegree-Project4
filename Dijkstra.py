cd91d33c151b67a832f3412ad100c8defa1ce4da
Kunal Dubey <kunaldubey2297@gmail.com>
2022-02-20 17:43:08 +0530

    DijkStra working!!!!!

--- Dijkstra.py
+++ Dijkstra.py
@@ -32,10 +32,15 @@ def find_shortest_path():
 
     node_c.join(node_b, 2)
     node_c.join(node_e, 1)
-    node_c.join(node_d, 1)
+    node_c.join(node_d, 1) 
+    node_c.join(node_a,4)
+    node_c.join(node_f,3)
 
     node_d.join(node_a, 2)
-    node_d.join(node_c, 1)
+    node_d.join(node_c, 1) 
+    
+    node_e.join(node_c,1)
+    node_e.join(node_f,2)
 
     unvisited.add('B')
     unvisited.add('C')
@@ -43,21 +48,47 @@ def find_shortest_path():
     unvisited.add('E')
     unvisited.add('F') 
     
-    to_visit = set()
+    to_visit = set() 
+    visited = set()
 
-    source_node = node_a 
-    
+    source_node = node_a  
+    result[node_a.name] = 0
+    visited.add(source_node)
     to_visit.add(source_node) 
     
     while len(to_visit) > 0:
-        current_node = to_visit.pop()
-        for child in current_node.children:  
-            if child not in result.keys():
-                result[child] = current_node.children[child]['distance'] 
-                to_visit.add(current_node.children[child]['node'])   
-            else:
-                if result[child] > current_node.children[child]['distance']:
-                    result[child] = current_node.children[child]['distance'] 
+        current_node : Node = to_visit.pop()  
+        visited.add(current_node.name)
+        current_node_children = current_node.children 
+        least_distance = 9999;  
+        least_distance_node = None;
+        for child in current_node_children: 
+            if child not in visited:
+                if current_node_children[child]['distance'] < least_distance:
+                    least_distance = current_node_children[child]['distance'] 
+                    least_distance_node = current_node_children[child]['node'] 
+            
+                if least_distance_node.name in result.keys(): 
+                    if result[least_distance_node.name] > least_distance + result[current_node.name] :
+                        result[least_distance_node.name] = least_distance + result[current_node.name]  
+                else:
+                    result[least_distance_node.name] = least_distance + result[current_node.name] 
+            
+        if least_distance_node is not None:
+            to_visit.add(least_distance_node)
+        
+        # for child in current_node_children:   
+        #     # child node is already present in the result dict  
+        #     if child in result.keys():
+        #         if result[child] > current_node_children[child]['distance'] + result[current_node.name]:
+        #             result[child] = current_node_children[child]['distance'] + result[current_node.name]
+        #     else:
+        #     # child node is not present in the result dict  
+        #         print("child is", child)
+        #         result[child] = current_node_children[child]['distance'] + result[current_node.name] 
+        #     if child not in visited:  
+        #         to_visit.add(current_node.children[child]['node'])
+            
          
              
 
