add12c3a735205129a92ddcddcd676b70f987024
Kunal Dubey <kunaldubey2297@gmail.com>
2022-02-22 01:55:19 +0530

    DFS REC WIP

--- dfs_rec.py
+++ dfs_rec.py
@@ -1,27 +1,85 @@
+from unittest import result
+
+
 class GraphNode:
     def __init__(self, data) -> None:
-         self.data = data
-         self.children = [] 
-    
-    def add_child(self,node):
+        self.data = data
+        self.children = []
+
+    def add_child(self, node):
         self.children.append(node)
-    
-    def remove_child(self,node):
-        self.children.remove(node) 
-    
+
+    def remove_child(self, node):
+        self.children.remove(node)
+
+
 class Graph:
-    
-    def __init__(self, root_value) -> None: 
-        self.root_node = GraphNode(root_value)
-        pass 
-    
+
+    def __init__(self, nodes) -> None:
+        self.nodes = nodes
+        self.dfs_rec_stack = []
+        self.dfs_visited = []
+        pass
+
     def add_edge(self, from_node: GraphNode, to_node: GraphNode):
-        from_node.add_child(to_node)
-        to_node.add_child(from_node) 
-    
-    def remove_edge(self, from_node:GraphNode, to_node: GraphNode):
-        from_node.remove_child(from_node)
-        to_node.remove_child(to_node)
-    
-    
-        
+        if from_node in self.nodes and to_node in self.nodes:
+            from_node.add_child(to_node)
+            to_node.add_child(from_node)
+
+    def remove_edge(self, from_node: GraphNode, to_node: GraphNode):
+        if from_node in self.nodes and to_node in self.nodes:
+            from_node.remove_child(from_node)
+            to_node.remove_child(to_node)
+
+    def recursive_search(self, node: GraphNode, search_value: str): 
+        # if node.data not in self.dfs_visited:
+        #     self.dfs_visited.append(node.data) 
+     
+        if node.data == search_value: 
+            print("Found the value", node)
+            return node
+        else: 
+            if node.data not in self.dfs_visited: 
+                self.dfs_visited.append(node.data)
+            if len(node.children) == 0:
+                return None
+            else: 
+                for child in node.children:
+                    if child.data not in self.dfs_visited:
+                        self.dfs_visited.append(child.data)
+                        return self.recursive_search(child, search_value) 
+              
+                
+
+
+# Creating a graph as above.
+nodeG = GraphNode('G')
+nodeR = GraphNode('R')
+nodeA = GraphNode('A')
+nodeP = GraphNode('P')
+nodeH = GraphNode('H')
+nodeS = GraphNode('S')
+
+graph1 = Graph([nodeS, nodeH, nodeG, nodeP, nodeR, nodeA])
+
+graph1.add_edge(nodeG, nodeR)
+graph1.add_edge(nodeA, nodeR)
+graph1.add_edge(nodeA, nodeG)
+graph1.add_edge(nodeR, nodeP)
+graph1.add_edge(nodeH, nodeG)
+graph1.add_edge(nodeH, nodeP)
+graph1.add_edge(nodeS, nodeR)
+
+
+for each in graph1.nodes:
+    print('parent node = ', each.data, end='\nchildren\n')
+    for each in each.children:
+        print(each.data, end=' ')
+    print('\n')
+
+print(graph1.recursive_search(nodeP, 'S'))
+# print(graph1.recursive_search(nodeG, 'A'))
+# assert nodeA.data == graph1.recursive_search(nodeG, 'A').data
+#assert nodeA.data == graph1.recursive_search(nodeS, 'A').data
+# assert nodeS.data == graph1.recursive_search(nodeP, 'S')
+# assert nodeR.data == graph1.recursive_search(nodeH, 'R').data
